0f02ad78c66157af75ff931b78159d59ec89b023
stiefel40k <stiefel40k@hotmail.com>
2017-03-17 13:29:47 +0000

    add code, initial catalog and README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Requirements #
+You need python3 installed (it might run on py2 as well, but I didn't test it)
+
+# What is this #
+So basically I needed a tool to learn for an MC-test so I wrote one. It shoots you with a question and the possibly answers and waits until you answer the question. After you did, it tells you if your answer was right or wrong, and if wrong, what would have been the right answer.
+
+# The catalog #
+The catalog is in JSON format.
+```
+{"questions":[
+    {"q":"<here comes the question>",
+    "answers":[
+      {"text":"<Answer one text>", "id":1},
+      {"text":"<Answer two text>", "id":2},
+      {"text":"<Answer three text>", "id":3},
+      {"text":"<Answer four text>", "id":4},
+      ...
+      {"text":"<Answer n text>", "id":n}
+    ],
+    "rightids":"<id of the right answers as one number>"}
+]}
+```
---
+++ mc_tester.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os.path
+import sys
+import json
+import random
+
+def print_question(questions):
+    qn = random.randint(0, len(questions) - 1)
+    question_text = questions[qn]['q']
+    answer_text = ''
+    for a in questions[qn]['answers']:
+        answer_text += str(a['id']) + ') ' + a['text'] + '\n'
+    print(question_text)
+    print(answer_text)
+    return questions[qn]['rightids']
+
+if len(sys.argv) != 2:
+    print('Usage: {0} <path_to_questions'.format(sys.argv[0]))
+    sys.exit(1)
+else:
+    with open(sys.argv[1], 'r') as f:
+        questions = json.loads(f.read())
+    # questions are read in. Inform the user about the usage:
+    print('The questions are coming. If you want to quit, please hit q and Enter')
+    print()
+    print('Please enter the correct answers sorted as one number')
+    print('So if you think answer 1, 2 and 4 are the right one, please enter "124"')
+    input('Press Enter to start')
+    print()
+    while True:
+        right_answer = print_question(questions['questions'])
+        choice = input(">>> ").lower().rstrip()
+        if choice == 'q':
+            break
+        else:
+            if right_answer == choice:
+                print('Your answer was right!')
+            else:
+                print('The right answer would have been: ' + right_answer + 
+                        ' your answer was: ' + choice)
+            print()
+    print('Thanks for using this software. Bye')
+    sys.exit(0)
